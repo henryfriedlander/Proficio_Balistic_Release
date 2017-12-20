afa63ce90ea5b96d938f039e106cc0c099ac76d9
IvaRStevens <ivastevens@gmail.com>
2017-12-20 14:13:40 -0500

    send state from exec

--- Executive/Executive.cpp
+++ Executive/Executive.cpp
@@ -1,6 +1,51 @@
 #include <stdio.h>
 #include "Dragonfly.h"
 #include "Dragonfly_config.h"
+#include "params.h"
+
+class TargetNode
+{
+  int  distance;
+  int direction;
+  int     width;
+  double  force;
+
+  public:
+    TargetNode(int dist, int dir, int wid, double frc)
+    {
+      distance = dist;
+      direction = dir;
+      width = wid;
+      force = frc;
+    };
+};
+
+vector<TargetNode> initTargetList( 
+  vector<int> dirs,
+  vector<int> forces,
+  vector<int> widths,
+  vector<double> distances)
+{
+  // assert widths/distances are the same length
+  if (distances.size() != widths.size())
+  {
+    throw Exception("Yaml error, widths and distances must have same length");
+  }
+  vector<TargetNode> targetList;
+
+  for (Iter forIt = forces.begin(); forIt!=forces.end(); ++forIt)
+  {
+    for (Iter dirIt = dirs.begin(); dirIt!=dirs.end(); ++dirIt)
+    {
+      for (int i = 0; i < distances.size(); i++)
+      {
+        TargetNode node = TargetNode(distances.begin()+i, *dirIt, widths.begin()+i, *forIt);
+        targetList.push_back(node);        
+      }
+    }
+  }
+  return targetList;
+};
 
 
 int main( int argc, char *argv[])
@@ -14,81 +59,114 @@ int main( int argc, char *argv[])
 		mod.Subscribe( MT_TRIAL_STATUS_FEEDBACK);
 		mod.Subscribe( MT_RT_POSITION_FEEDBACK);
 		mod.Subscribe( MT_TASK_STATE_CONFIG);
-		
-		// TODO -- here is where one should read from the .config file and send out the next 
-    // specifications for the next trial instead randomly selecting
-		
-		MDF_TRIAL_INPUT trial_input_data;
-		double targetPositions [4] = {0.10, 0.15, 0.20};
-		double forceThresholds [4] = {0.01, 0.02, 0.03, 0.04};
-		int directions [3] = {0,1,2};
-		int UpOrDownDirs [2] = {-1,1};
-		
-		double tDist = targetPositions[rand() % 3];
-		double tError = 0.03;
-		
-		int UpOrDown = UpOrDownDirs[rand() % 2];
-		trial_input_data.XorYorZ = directions[rand() % 2];
-		trial_input_data.forceThreshold = forceThresholds[rand() % 4];
-		trial_input_data.targetDistance = tDist;
-		trial_input_data.UpOrDown = UpOrDown;
-		trial_input_data.targetError = tError;
-		CMessage trial_input_M( MT_TRIAL_INPUT);
-		trial_input_M.SetData( &trial_input_data, sizeof(trial_input_data));
-		mod.SendMessageDF( &trial_input_M);
-    
-		cout << "XorYorZ " << trial_input_data.XorYorZ << endl << "force threshold " << 
-      trial_input_data.forceThreshold  << endl << "target distance " << 
-      trial_input_data.targetDistance << endl << "UpOrDown " << 
-      trial_input_data.UpOrDown << endl;
-		cout << "Sent out data" << endl;
-		
-        std::cout << "Consumer running...\n" << std::endl;
-        
-		while( 1) 
-		{
+		mod.Subscribe( MT_BURT_STATUS);
+
+    // ===================================================================================
+    int currentState = START;
+    int nextState = (currentState % (nStates)) + 1;
+    bool userDefState = false;
+    bool userDefTarget = false;
+    int nStates = 6;
+    bool rewardable = false;
+    bool shouldReset = false;
+    double tError = 0.03;
+
+    // Read in yaml file
+
+    // Initialize targetList
+    vector<TargetNode> targetList = initTargetList(directions, forces, widths, distances);
+    Iter targetIter = targetList.begin();
+    TargetNode currentTarget = *targetIter;
+    TargetNode nextTarget = NULL;
+
+    // Run the experiment
+    while(1)
+    {
       CMessage M;
 			mod.ReadMessage( &M);
-			//std::cout << "Received message " << M.msg_type << std::endl;
-            
-			switch( M.msg_type)
+
+      // Check for new messages
+      switch( M.msg_type)
       {
-				case MT_TASK_STATE_CONFIG:
-					MDF_TASK_STATE_CONFIG task_state_data;
-					M.GetData( &task_state_data);
-					if(strcmp(task_state_data.fdbk_display_color, "yellow") == 0)
-          {
-						// populate messages
-						// TODO read from config file to populate messages
-						trial_input_data.XorYorZ = directions[rand() % 2];
-						trial_input_data.forceThreshold = forceThresholds[rand() % 4];
-						trial_input_data.targetDistance = targetPositions[rand() % 3];
-						trial_input_data.UpOrDown = UpOrDownDirs[rand() % 2];
-						trial_input_data.targetError = 0.03;
-						
-						cout << "XorYorZ " << trial_input_data.XorYorZ << endl << "force threshold " 
-              << trial_input_data.forceThreshold  << endl << "target distance " 
-              << trial_input_data.targetDistance << endl << "UpOrDown " 
-              << trial_input_data.UpOrDown << endl;
-						cout << "Sent out data" << endl;
-						
-						MDF_TASK_STATE_CONFIG task_state_data;
-						task_state_data.target[0] = (trial_input_data.targetDistance - trial_input_data.targetError) * 1240 / 0.2;
-						task_state_data.target[1] = (trial_input_data.targetDistance + trial_input_data.targetError) * 1240 / 0.2;
-						task_state_data.direction = trial_input_data.UpOrDown + trial_input_data.XorYorZ;
-						
-						// Send out messages
-						CMessage trial_input_M( MT_TRIAL_INPUT);
-						trial_input_M.SetData( &trial_input_data, sizeof(trial_input_data));
-						mod.SendMessageDF( &trial_input_M);
-						
-						CMessage task_state_config_M( MT_TASK_STATE_CONFIG);
-						task_state_config_M.SetData( &task_state_data, sizeof(task_state_data));
-						mod.SendMessageDF( &task_state_config_M);
-					}
-					break;
-			}
-		}
+        // This should probably be a different message
+        case MT_TASK_STATE_CONFIG:
+          break;
+
+        /* interrupt flag was sent
+        case MT_FLAG:
+          break;
+        */
+
+        // BURT sent an msg about state termination.
+        case MT_BURT_STATUS:
+          MDF_BURT_STATUS burt_status_data;
+					M.GetData( &burt_status_data);
+          // if there is a success for given state
+          if (burt_status_data.task_complete)
+          {// move to the next state
+            if (burt_status_data.task_success)
+            {
+              shouldReset = false;
+              if (!userDefState)
+              { // progress next state as usual if not set else where
+                nextState = (currentState % (nStates)) + 1;
+              }
+            // Subject error occurred, start next trial, no reward
+            } else {
+              shouldReset = true;
+              rewardable = false;
+              break;
+            }
+            // pull up next TARGET parameters
+            if ( nextTarget == NULL)
+            {
+              currentTarget = targetIter++;
+            } else {
+              currentTarget = nextTarget;
+            }
+
+            // determine next state      
+            if ( nextState ==  RESET || shouldReset)
+            { // Determine next target (send out with state)
+
+              // Send out RESET messages and next target parameters
+              CMessage trial_input_M( MT_TRIAL_INPUT);
+              trial_input_M.SetData( &trial_input_data, sizeof(trial_input_data));
+              mod.SendMessageDF( &trial_input_M);
+
+              CMessage task_state_config_M( MT_TASK_STATE_CONFIG);
+              task_state_config_M.SetData( &task_state_data, sizeof(task_state_data));
+              mod.SendMessageDF( &task_state_config_M);      
+
+              // Set next Target
+              trial_input_data.direction = currentTarget.direction;
+              trial_input_data.forceThreshold = currentTarget.force;
+              trial_input_data.targetDistance = currentTarget.distance;
+              trial_input_data.width = currentTarget.width;
+              trial_input_data.targetError = tError;
+
+              // Print target definition
+              cout << "XorYorZ " << trial_input_data.XorYorZ << endl << "force threshold " 
+                << trial_input_data.forceThreshold  << endl << "target distance " 
+                << trial_input_data.targetDistance << endl << "UpOrDown " 
+                << trial_input_data.UpOrDown << endl;
+              cout << "Sent out data" << endl;
+
+              // If this is a rewardable transition, do so.
+              if (!shouldReset) {
+                cout << "REWARD!!!" << endl
+              }
+
+              // Reset variables
+              shouldReset = false;
+              userDefState = false;
+              userDefTarget = false;
+              nextTarget = NULL;
+            }            
+          }
+      }
+    }
+    // ===================================================================================
 	}
 	catch( UPipeClosedException &e)
 	{
@@ -113,6 +191,6 @@ int main( int argc, char *argv[])
 		MyCString s;
 		std::cout << "Unknown Exception!" << std::endl;
 	}
-	
+
 	std::cout << "Exiting cleanly." << std::endl;
 }
